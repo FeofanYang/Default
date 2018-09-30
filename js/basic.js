a99aba41eb9ef3c02ca7c277ce777d5b910d8b91
FeofanYang <yyf0326@qq.com>
2018-09-30 15:18:53 +0800

    重写HTML CSS JS REG
    统一代码规范性，部分细节改善

--- js/basic.js
+++ js/basic.js
@@ -1,233 +1,212 @@
-/**
- * Version - 1.0
- *
- * Copyright (c) 2017 Feofan Yang
- *
- * Update
- * [1.] 增加按钮倒计时函数
- * [2.] 快速添加Reg组件功能
- *
- */
-
-var setFontSize = false;
-var showPage = new Function();
-var myUtil = {};
+var bSetRem = false;
+var fnShowPage = new Function();
+var oConst = {
+	vertify: {
+		isName: function (str) {
+			var oReg = /^[\u4E00-\u9FA5]{2,6}$/;
+			return oReg.test(str);
+		},
+		isTel: function (str) {
+			var oReg = /^1[34578][0-9]\d{8}$/;
+			return oReg.test(str);
+		},
+		isIDCard: function (str) {
+			var oReg = new RegExp("/(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/");
+			return oReg.test(str);
+		}
+	},
+};
 
 (function (doc, win) {
-	var docEl = doc.documentElement,
-		resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-		recalc = function () {
-			var clientWidth = docEl.clientWidth;
-			if (!clientWidth) return;
-			docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
-			setFontSize = true;
-		};
+	var docEl = doc.documentElement, resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize', recalc = function () {
+		var clientWidth = docEl.clientWidth;
+		if (!clientWidth) return;
+		docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+		bSetRem = true;
+	}
 	if (!doc.addEventListener) return;
 	win.addEventListener(resizeEvt, recalc, false);
 	doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
 
-function lazyLoad(isResize, $ele, callback) {
-	var curNum = 0;
-	var parent;
-	var preLoadNum;
+function fnLazyLoad(argResize, argEl, argCallback) {
+	var nCurCount = 0;
+	var oImgs;
+	var nLoadCount;
 	if (arguments[1]) {
-		parent = $ele.find('.preload');
-		preLoadNum = $ele.find('.preload').length;
+		oImgs = $(argEl).find('.preload');
+		nLoadCount = $(argEl).find('.preload').length;
 	} else {
-		parent = $('body').find('.preload');
-		preLoadNum = $('body').find('.preload').length;
+		oImgs = $('body').find('.preload');
+		nLoadCount = $('body').find('.preload').length;
 	}
-	parent.each(function () {
-		var self = $(this);
-		if (self.attr('data-src')) {
-			var img = new Image();
-			img.src = self.attr('data-src');
-			img.onload = function () {
-				curNum++;
-				self.attr('src', self.attr('data-src'));
-				if (isResize && !self.hasClass('noResize')) {
-					self.css({ 'width': this.width / 100 + 'rem' });
+	oImgs.each(function () {
+		var that = $(this);
+		if (that.attr('data-src')) {
+			var oImg = new Image();
+			oImg.src = that.attr('data-src');
+			oImg.onload = function () {
+				nCurCount++;
+				that.attr('src', that.attr('data-src'));
+				if (argResize && !that.hasClass('el_noR')) {
+					that.css({ 'width': this.width / 100 + 'rem' });
 				}
-				if (self.hasClass('_sCenter')) {
-					self.css({ 'left': '50%' });
-					self.css({ 'margin-left': -this.width / 100 / 2 + 'rem' });
+				if (that.hasClass('img_C')) {
+					that.css({ 'position': 'absolute', 'left': '50%', 'margin-left': -this.width / 100 / 2 + 'rem' });
 				}
-				if (typeof callback == 'function') {
-					if (curNum == preLoadNum) {
-						callback();
+				if (that.hasClass('img_M')) {
+					that.css({ 'position': 'absolute', 'top': '50%', 'margin-top': -this.height / 100 / 2 + 'rem' });
+				}
+				if (that.hasClass('img_O')) {
+					that.css({ 'position': 'absolute', 'left': '50%', 'margin-left': -this.width / 100 / 2 + 'rem', 'top': '50%', 'margin-top': -this.height / 100 / 2 + 'rem' });
+				}
+				if (typeof argCallback == 'function') {
+					if (nCurCount == nLoadCount) {
+						argCallback();
 					}
 				} else {
-					var percent = curNum / preLoadNum;
-					loadEnd(percent);
+					var nLoadPercent = nCurCount / nLoadCount;
+					$('.page_preload .loader_text').html(parseInt(nLoadPercent * 100, 10) + '%');
+					// preload end
+					if (nLoadPercent == 1 && bSetRem) {
+						$('.page_preload').animate({ 'opacity': 0 }, 500, function () {
+							$('.page_preload').remove();
+							fnShowPage();
+						});
+					}
 				}
-			};
-			img.onerror = function (data) {
-				var index = img.src.lastIndexOf('/');
-				var imgname = img.src.substr(index + 1);
-				pWidget.alert("图片加载错误：" + imgname);
+			}
+			oImg.onerror = function (data) {
+				var nImgIndex = oImg.src.lastIndexOf('/');
+				var sImgName = oImg.src.substr(nImgIndex + 1);
+				oWidget.alert("图片缓存错误：" + sImgName);
 			}
 		}
 	});
 }
 
-function loadEnd(percent) {
-	$('.loading-percent').html(parseInt(percent * 100, 10) + '%');
-	$('.loading-percent').animate({ 'opacity': 1 }, 100);
-	if (percent == 1 && setFontSize) {
-		$('#loading').animate({ 'opacity': 0 }, 500, function () {
-			$('#loading').remove();
-			showPage();
-		});
-	}
-}
-
-function getQueryString(name) {
-	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-	var r = window.location.search.substr(1).match(reg);
-	if (r !== null) {
-		return unescape(r[2]);
-	}
-	return null;
-}
-
-function btnTimer(btnId, btnCount, btnHint) { /* [1.] */
-	var btnVal = $(btnId).val();
-	$(btnId).attr("disabled", "disabled");
-	typeof btnHint == 'string' ? $(btnId).attr("value", btnCount + "S" + btnHint) : $(btnId).attr("value", btnCount + "S");
-	var btnInterval = setInterval(function () {
-		btnCount--;
-		typeof btnHint == 'string' ? $(btnId).attr("value", btnCount + "S" + btnHint) : $(btnId).attr("value", btnCount + "S");
-		if (btnCount < 0) {
-			$(btnId).removeAttr("disabled");
-			clearInterval(btnInterval);
-			$(btnId).attr("value", btnVal);
-		}
-	}, 1000);
-}
-
-myUtil.vertify = {
-	isName: function (str) {
-		var reg = /^[\u4E00-\u9FA5]{2,6}$/;
-		return reg.test(str);
-	},
-	isTel: function (str) {
-		var reg = /^1[34578][0-9]\d{8}$/;
-		return reg.test(str);
-	},
-	isIDCard: function (str) {
-		var reg = new RegExp("/(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/");
-		return reg.test(str);
-	}
-};
-
-var pWidget = {
+var oWidget = {
 	params: {},
 	config: function (params) {
 		this.params = params;
-		if (params.music) {
-			this.addMusic();
-		}
 		if (params.arrow) {
 			this.addArrow();
 		}
+		if (params.music) {
+			this.addMusic();
+		}
 	},
-	addMusic: function () {
-		var music = null;
-		var openImgUrl = 'images/global/music-open.png';
-		var closeImgUrl = 'images/global/music-close.png';
-		if (typeof this.params.musicImgUrl == 'object') {
-			openImgUrl = this.params.musicImgUrl[0];
-			closeImgUrl = this.params.musicImgUrl[1];
+	alert: function (argWords, argConfirm, argCallback) {
+		if ($('.page_alert').length > 0) {
+			$('.page_alert').remove();
 		}
-		if ($('#musicBtn').length > 0) {
-			music = $('#music')[0];
-		} else {
-			$('body').prepend('<a href="javascript:;" class="musicBtn music_open" id="musicBtn"></a>');
-			$('body').prepend('<audio src="mp3/music.mp3" id="music" autoplay loop></audio>');
-			$('#musicBtn').css('background-image', 'url(' + openImgUrl + ')');
-			music = $('#music')[0];
+		var sConfirm = '确定';
+		if (typeof argConfirm == 'string') {
+			sConfirm = argConfirm;
 		}
-		$('#musicBtn').bind('click', function () {
-			if (music.paused) {
-				music.play();
-				$('#musicBtn').addClass('music_open');
-				$('#musicBtn').css('background-image', 'url(' + openImgUrl + ')');
-			} else {
-				music.pause();
-				$('#musicBtn').removeClass('music_open');
-				$('#musicBtn').css('background-image', 'url(' + closeImgUrl + ')');
+		$('body').append('<div class="mask_css page_alert"><div class="alert_container"><div class="alert_title">' + argWords + '</div><div class="alert_confirm">' + sConfirm + '</div></div></div>');
+		$('.alert_confirm').bind('click', function () {
+			$('.page_alert').remove();
+			if (typeof argCallback == 'function') {
+				argCallback();
 			}
 		});
 	},
 	addArrow: function () {
-		if ($('#arrow').length > 0) {
-			$('#arrow').show();
+		if ($('._arrow').length > 0) {
+			$('._arrow').show();
 		} else {
 			if (typeof this.params.arrowImgUrl == 'string') {
-				$('body').append('<img src="' + this.params.arrowImgUrl + '" class="arrow" id="arrow">');
+				$('body').append('<img src="' + this.params.arrowImgUrl + '" class="_arrow">');
 			} else {
-				$('body').append('<img src="images/global/arrow.png" class="arrow" id="arrow">');
+				$('body').append('<img src="images/global/arrow.png" class="_arrow">');
 			}
 		}
 	},
-	addReg: function (selector, hasMsgCode) { /* [2.] */
-		if (typeof hasMsgCode == 'boolean' && hasMsgCode == true) {
-			$(selector).append("<div id='Rmask'><div class='regsiter'><p id='Rtitle'>登记您的资料<i class='iconfont icon-guanbi2' id='Rclose'></i> </p><div class='inputWrap'><input type='text' placeholder='姓名' id='Rname' class='name'><i class='name-icon iconfont'></i></div><div class='inputWrap'><input type='tel' placeholder='电话' id='Rtel' class='phone'><i class='tel-icon iconfont'></i></div><div class='inputWrap'><input type='tel' placeholder='验证码' id='Ryzm'><input type='button' value='发送验证码' id='Rsend'></div><p id='Rhint'>&nbsp;</p><input type='button' value='提交' id='Rsubmit'></div></div>");
+	addMusic: function () {
+		var oMusic = null;
+		var sBGMOpen = 'images/global/music-open.png';
+		var sBGMClose = 'images/global/music-close.png';
+		if (typeof this.params.musicImgUrl == 'object') {
+			sBGMOpen = this.params.musicImgUrl[0];
+			sBGMClose = this.params.musicImgUrl[1];
 		}
-		else {
-			$(selector).append("<div id='Rmask'><div class='regsiter'><p id='Rtitle'>登记您的资料<i class='iconfont icon-guanbi2' id='Rclose'></i> </p><div class='inputWrap'><input type='text' placeholder='姓名' id='Rname' class='name'><i class='name-icon iconfont'></i></div><div class='inputWrap'><input type='tel' placeholder='电话' id='Rtel' class='phone'><i class='tel-icon iconfont'></i></div><p id='Rhint'>&nbsp;</p><input type='button' value='提交' id='Rsubmit'></div></div>");
-		}
-	},
-	showShare: function (canClose) {
-		function addSwitch() {
-			if (typeof canClose == 'boolean' && !canClose) {
-				$('#fxzy').unbind('click')
-			} else {
-				$('#fxzy').bind('click', function () {
-					$('#fxzy').hide();
-				});
-			}
-		}
-		if ($('#fxzy').length > 0) {
-			$("#fxzy").show();
-			addSwitch();
+		if ($('.bgm_btn').length > 0) {
+			oMusic = $('#music')[0];
 		} else {
-			if (typeof this.params.shareImage == 'string') {
-				$('body').prepend('<img id="fxzy" src="' + this.params.shareImage + '" style="position:fixed;top: 0;width:100%;height:100%;z-index:9999;">');
+			$('body').prepend('<a href="javascript:;" class="bgm_btn am_bgm"</a>');
+			$('.bgm_btn').css('background-image', 'url(' + sBGMOpen + ')');
+			oMusic = $('#music')[0];
+		}
+		$('.bgm_btn').bind('click', function () {
+			if (oMusic.paused) {
+				oMusic.play();
+				$('.bgm_btn').addClass('am_bgm');
+				$('.bgm_btn').css('background-image', 'url(' + sBGMOpen + ')');
 			} else {
-				$('body').prepend('<img id="fxzy" src="images/global/fxzy.png" style="position:fixed;top: 0;width:100%;height:100%;z-index:9999;">');
-			}
-			addSwitch();
-		}
-	},
-	hideShare: function () {
-		$('#fxzy').hide();
-	},
-	showLoading: function () {
-		if ($(".loadingPage").length > 0) {
-			$(".loadingPage").show();
-		} else {
-			$("body").prepend("<div class='aMask loadingPage'><div class='ball_spread'></div><div class='ball_shrink'></div></div>");
-		}
-	},
-	hideLoading: function () {
-		$(".loadingPage").hide();
-	},
-	alert: function (res, callback, okres) {
-		if ($('.myalert').length > 0) {
-			$('.myalert').remove();
-		}
-		var ok = '确定';
-		if (typeof okres == 'string') {
-			ok = okres;
-		}
-		$('body').append('<div class="myalert"><div class="alert-mask"></div><div class="_alert"><div class="alert-title">' + res + '</div><div class="alert-ok">' + ok + '</div></div></div>');
-		$('.alert-ok').bind('click', function () {
-			$('.myalert').remove();
-			if (typeof callback == 'function') {
-				callback();
+				oMusic.pause();
+				$('.bgm_btn').removeClass('am_bgm');
+				$('.bgm_btn').css('background-image', 'url(' + sBGMClose + ')');
 			}
 		});
 	},
-};
+	addReg: function (argEl, argMsgCode) {
+		$('body').append();
+		if (typeof argMsgCode == 'boolean' && argMsgCode == true) {
+			$(argEl).append('<div class="mask_css page_reg"><div class="reg_container"><button class="reg_btn_close">×</button><div class="reg_title">登记您的资料</div><input class="reg_input_name" type="text" placeholder="姓名"><p class="reg_error_name">× 请输入2-6位的中文姓名</p><input class="reg_input_tel" type="tel" placeholder="电话"><p class="reg_error_tel">× 请输入正确的手机号码</p><input class="reg_input_code" type="text" placeholder="验证码"><button class="reg_btn_code">获取验证码</button><p class="reg_error_code">× 请输入验证码</p><button class="reg_btn_submit">提交</button></div></div>');
+		}
+		else {
+			$(argEl).append('<div class="mask_css page_reg"><div class="reg_container"><button class="reg_btn_close">×</button><div class="reg_title">登记您的资料</div><input class="reg_input_name" type="text" placeholder="姓名"><p class="reg_error_name">× 请输入2-6位的中文姓名</p><input class="reg_input_tel" type="tel" placeholder="电话"><p class="reg_error_tel">× 请输入正确的手机号码</p><button class="reg_btn_submit">提交</button></div></div>');
+		}
+	},
+	addShare: function (argClose) {
+		function addSwitch() {
+			if (typeof argClose == 'boolean' && !argClose) {
+				$('.page_share').unbind('click')
+			} else {
+				$('.page_share').bind('click', function () {
+					$('.page_share').fadeOut();
+				});
+			}
+		}
+		if ($('.page_share').length > 0) {
+			$(".page_share").fadeIn();
+			addSwitch();
+		} else {
+			if (typeof this.params.shareImgUrl == 'string') {
+				$('body').prepend('<div class="mask_css page_share"><img class="shareImg" src="' + this.params.shareImgUrl + '"></div>');
+			} else {
+				$('body').prepend('<div class="mask_css page_share"><img class="shareImg" src="images/global/shareImg.png"></div>');
+			}
+			addSwitch();
+		}
+	},
+	btnTimer: function (argId, argCount, argHint) {
+		var sBtnVal = $(argId).val();
+		$(argId).attr("disabled", "disabled");
+		typeof argHint == 'string' ? $(argId).html(argCount + "秒后" + argHint) : $(argId).html(argCount + "秒");
+		var nBtnInterval = setInterval(function () {
+			argCount--;
+			typeof argHint == 'string' ? $(argId).html(argCount + "秒后" + argHint) : $(argId).html(argCount + "秒");
+			if (argCount < 0) {
+				$(argId).removeAttr("disabled");
+				clearInterval(nBtnInterval);
+				$(argId).html(sBtnVal);
+			}
+		}, 1000);
+	},
+	showLoading: function (argText) {
+		if ($(".page_loading").length > 0) {
+			$(".page_loading").fadeIn();
+		} else {
+			if (typeof argText == 'string') {
+				$("body").prepend('<div class="mask_css page_loading"><div class="loader"></div><p class="loader_text el_text">' + argText + '</p></div>');
+			} else {
+				$("body").prepend('<div class="mask_css page_loading"><div class="loader"></div></div>');
+			}
+		}
+	},
+	hideLoading: function () {
+		$(".page_loading").fadeOut();
+	},
+}
--- js/reg.js
+++ js/reg.js
@@ -1,91 +1,99 @@
-//登记框
-var vertify = {
-	isName: function (str) {
-		var reg = /^[\u4E00-\u9FA5]{2,6}$/;
-		return reg.test(str);
-	},
-	isTel: function (str) {
-		var reg = /^1[34578][0-9]\d{8}$/;
-		return reg.test(str);
-	},
-	isIDCard: function (str) {
-		var reg = new RegExp("/(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/");
-		return reg.test(str);
-	}
-};
-$('body').on('focus', '#Rname', function () {
-	$('.name-icon').removeClass('icon-guanbi2fill icon-yuanxingxuanzhongfill');
-	$(this).css("border", "1px solid #333");
+let vertify = oConst.vertify;
+
+// 失去焦点时验证输入框
+$('body').on('focus', '.reg_input_name', function () {
+	$(this).css("border", "1px solid #1AAD19");
 });
-$('body').on('blur', '#Rname', function () {
-	var name = $("#Rname").val();
+$('body').on('blur', '.reg_input_name', function () {
+	let name = $(".reg_input_name").val();
 	$(this).removeAttr("style");
 	if (!vertify.isName(name)) {
-		$('.name-icon').addClass('icon-guanbi2fill');
-		$("#Rhint").text("*姓名请输入2-6位的中文");
+		$(".reg_error_name").fadeIn();
 		return false;
 	} else {
-		$('.name-icon').addClass('icon-yuanxingxuanzhongfill');
-		$("#Rhint").text("")
+		$(".reg_error_name").fadeOut();
 	}
 });
-$('body').on('focus', '#Rtel', function () {
-	$('.tel-icon').removeClass('icon-guanbi2fill icon-yuanxingxuanzhongfill');
-	$(this).css("border", "1px solid #333");
+
+$('body').on('focus', '.reg_input_tel', function () {
+	$(this).css("border", "1px solid #1AAD19");
 });
-$('body').on('blur', '#Rtel', function () {
-	var tel = $("#Rtel").val();
+$('body').on('blur', '.reg_input_tel', function () {
+	let tel = $(".reg_input_tel").val();
 	$(this).removeAttr("style");
 	if (!vertify.isTel(tel)) {
-		$('.tel-icon').addClass('icon-guanbi2fill');
-		$("#Rhint").text("*请输入正确的手机号码");
+		$(".reg_error_tel").fadeIn();
 		return false;
 	} else {
-		$('.tel-icon').addClass('icon-yuanxingxuanzhongfill');
-		$("#Rhint").text("")
+		$(".reg_error_tel").fadeOut();
 	}
 });
-$('body').on('click', '#Rsend', function () {
-	btnTimer($("#Rsend"), 60, "再次获取");
+
+$('body').on('focus', '.reg_input_code', function () {
+	$(this).css("border", "1px solid #1AAD19");
 });
-//登记框提交
-$('body').on('click', '#Rsubmit', function () {
-	var name = $("#Rname").val();
-	var tel = $("#Rtel").val();
-	if (!vertify.isName(name)) {
-		pWidget.alert("姓名请输入2-6位的中文");
+$('body').on('blur', '.reg_input_code', function () {
+	let code = $(".reg_input_code").val();
+	$(this).removeAttr("style");
+	if (code == "") {
+		$(".reg_error_code").fadeIn();
 		return false;
+	} else {
+		$(".reg_error_code").fadeOut();
+	}
+});
+
+// 获取验证码
+$('body').on('click', '.reg_btn_code', function () {
+	oWidget.btnTimer($(".reg_btn_code"), 60, "重新获取");
+});
+
+// 登记框提交
+$('body').on('click', '.reg_btn_submit', function () {
+	let bSubmit = true;
+	let name = $(".reg_input_name").val();
+	let tel = $(".reg_input_tel").val();
+	console.log('获取的姓名：' + name);
+	console.log('获取的电话：' + tel);
+	if (!vertify.isName(name)) {
+		$(".reg_error_name").hide();
+		$(".reg_error_name").fadeIn();
+		bSubmit = false;
 	}
 	if (!vertify.isTel(tel)) {
-		pWidget.alert("请输入正确的手机号码");
-		return false;
+		$(".reg_error_tel").hide();
+		$(".reg_error_tel").fadeIn();
+		bSubmit = false;
 	}
-	if ($("#Ryzm")) {
-		console.log('yzm!!!');
-		var yzm = $("#Ryzm").val();
-		if (yzm == "") {
-			pWidget.alert("请输入验证码");
-			return false;
+	if ($(".reg_input_code")) {
+		let code = $(".reg_input_code").val();
+		if (code == "") {
+			$(".reg_error_code").hide();
+			$(".reg_error_code").fadeIn();
+			bSubmit = false;
 		}
 	}
-	//验证通过 显示loading
-	pWidget.showLoading();
-	$.ajax({
-		url: 'dal/AddInfo.aspx',
-		type: 'post',
-		data: { name: name, tel: tel },
-		success: function (res) {
-			//ajax成功 隐藏loading
-			pWidget.hideLoading();
-			if (res == 1) {
-				alert("登记成功");
-			} else if (res == -1) {
-				alert("网络延迟，请稍后再试");
+	if (bSubmit) {
+		// 验证通过 显示loading
+		oWidget.showLoading();
+		$.ajax({
+			url: 'dal/AddInfo.aspx',
+			type: 'post',
+			data: { name: name, tel: tel },
+			success: function (res) {
+				// 隐藏loading
+				oWidget.hideLoading();
+				if (res == 1) {
+					alert("登记成功");
+				} else if (res == -1) {
+					alert("网络延迟，请稍后再试");
+				}
 			}
-		}
-	});
+		});
+	}
 });
-//登记框关闭
-$('body').on('click', '#Rclose', function () {
-	$("#Rmask").fadeOut();
+
+// 登记框关闭
+$('body').on('click', '.reg_btn_close', function () {
+	$(".page_reg").fadeOut();
 });
